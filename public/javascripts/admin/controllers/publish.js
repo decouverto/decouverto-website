3f29489ff036cc59bbd9d2592ea0d6a2a83f4f1e
Cédric JUNG <cedced19@gmail.com>
2018-07-12 10:55:29 +0200

    Add preview for JSON file

--- public/javascripts/admin/controllers/publish.js
+++ public/javascripts/admin/controllers/publish.js
@@ -1,3 +1,30 @@
-module.exports = ['$scope', '$http', '$rootScope', 'notie', function ($scope, $http, $rootScope, notie) {
+module.exports = ['$scope', '$http', '$rootScope', 'Upload', 'notie', function ($scope, $http, $rootScope, Upload, notie) {
+    $scope.uploading = false;
+    $scope.preview = false;
 
+    $scope.upload = function () {
+        var reader = new FileReader();
+        reader.readAsText($scope.desc);
+        reader.onload = function() {
+            try {
+                var obj = JSON.parse(reader.result);
+                console.log(obj)
+            } catch (e) {
+                notie.alert(3, 'Fichier JSON invalide.', 3);
+                $scope.uploading = false;
+            }
+        };
+    }
+
+    $scope.generatePreview = function () {
+        var reader = new FileReader();
+        reader.readAsText($scope.desc);
+        reader.onload = function() {
+            try {
+                $scope.preview = JSON.stringify(JSON.parse(reader.result), null, '\t');
+            } catch (e) {
+                $scope.preview = false;
+            }
+        };
+    }
 }];
