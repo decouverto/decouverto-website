dbb89adc349431351079140a6d2bdb0077a2aaf3
Cédric JUNG <cedced19@gmail.com>
2018-07-13 20:57:46 +0200

    Keep original filename

--- routes/walks-api.js
+++ routes/walks-api.js
@@ -1,39 +1,49 @@
 var express = require('express');
 var router = express.Router();
 var auth = require('../policies/auth.js');
-var path= require('path');
-var upload = require('multer')({ dest: './walks/' })
+var path = require('path');
+var multer = require('multer');
+var upload = multer({
+    storage: multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, './walks/');
+        },
+        filename: function (req, file, cb) {
+            cb(null, file.originalname);
+        }
+    })
+});
 
 /* GET Walks */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     res.json(req.app.walks.getAll());
 });
 
 /* GET Walk */
-router.get('/:id', function(req, res, next) {
+router.get('/:id', function (req, res, next) {
     req.app.walks.get('id', req.params.id, function (err, model) {
-        if(err) return next(err);
+        if (err) return next(err);
         res.json(model);
     });
 });
 
 /* POST Walk */
-router.post('/', auth, upload.single('file'), function(req, res, next) {
+router.post('/', auth, upload.single('file'), function (req, res, next) {
     if (req.file === undefined) {
         err = new Error('You must upload a file.');
         err.status = 400;
         return next(err);
     }
     req.app.walks.post(req.body, function (err) {
-        if(err) return next(err);
+        if (err) return next(err);
         res.json({ status: true });
     });
 });
 
 /* DELETE Walk */
-router.delete('/:id', auth, function(req, res, next) {
+router.delete('/:id', auth, function (req, res, next) {
     req.app.walks.delete('id', req.params.id, function (err) {
-        if(err) return next(err);
+        if (err) return next(err);
         res.json({ status: true });
     });
 });
