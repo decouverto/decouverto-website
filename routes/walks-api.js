4bbff984e9b0c3d89c8d48cf246d642704f51598
Cédric JUNG <cedced19@gmail.com>
2018-07-14 21:42:54 +0200

    Delete zip file when the walk is deleted

--- routes/walks-api.js
+++ routes/walks-api.js
@@ -2,11 +2,13 @@ var express = require('express');
 var router = express.Router();
 var auth = require('../policies/auth.js');
 var path = require('path');
+var fs = require('fs');
+var existsFile = require('exists-file');
 var multer = require('multer');
 var upload = multer({
     storage: multer.diskStorage({
         destination: function (req, file, cb) {
-            cb(null, './walks/');
+            cb(null, path.resolve(__dirname, '../walks/'));
         },
         filename: function (req, file, cb) {
             cb(null, file.originalname);
@@ -45,6 +47,12 @@ router.delete('/:id', auth, function (req, res, next) {
     req.app.walks.delete('id', req.params.id, function (err) {
         if (err) return next(err);
         res.json({ status: true });
+        var p = path.resolve(__dirname, '../walks/', (req.params.id + '.zip')) 
+        existsFile(p, function (err, exists) {
+            if (!err && exists) {
+                fs.unlink(p, function (){});
+            }
+        });
     });
 });
 
