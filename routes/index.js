223c834e60d70b62456501cfc28acbb429e917cd
Cédric JUNG <cedced19@gmail.com>
2018-07-16 11:19:52 +0200

    Add  walk view

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,17 @@ var auth = require('../policies/auth.js');
 /* GET home page */
 router.get('/', function (req, res, next) {
     res.locals.walks = req.app.walks.getAll();
-    res.render('index');    
+    res.render('index');
 });
+
+/* GET walk preview page */
+router.get('/:id', function (req, res, next) {
+    req.app.walks.get('id', req.params.id, function (err, data) {
+        if (err) return next(err);
+        res.locals.walk = data;
+        res.render('walk');
+    });
+});
+
 
 module.exports = router;
