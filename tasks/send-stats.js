d5ac4199f2426dde1c5dddfe4b3b06e703b50269
Cédric JUNG <cedced19@gmail.com>
2018-07-20 15:52:26 +0200

    List walks in mail

--- tasks/send-stats.js
+++ tasks/send-stats.js
@@ -14,7 +14,24 @@ moment.locale('fr');
 module.exports = function (walks, users, cb) {
 
     // Generate text
-    var text = `<h1>Résumé de la semaine</h1>`;
+    var text = `<h1>Stats de la semaine n° ${moment().format('W')} de ${moment().format('YYYY')}</h1>
+    <table style="border-collapse: collapse;"><tbody>
+    <tr  style="background-color: #dc3133; font-weight:bold; font-size:1em; line-height:1.2em; color: #fff;">
+        <th style="padding: 8px;border: solid 1px black;">Id</th>
+        <th style="padding: 8px;border: solid 1px black;">Nom</th>
+        <th style="padding: 8px;border: solid 1px black;">Téléchargement dans l'application</th>
+        <th style="padding: 8px;border: solid 1px black;">Aperçu sur Internet</th>
+    </tr>`
+
+    walks.forEach(function (walk) {
+        text += `<tr>
+            <td style="padding: 8px;border: solid 1px black;">${walk.id}</td>
+            <td style="padding: 8px;border: solid 1px black;">${walk.title}</td>
+            <td style="padding: 8px;border: solid 1px black;">${walk.app}</td>
+            <td style="padding: 8px;border: solid 1px black;">${walk.web}</td>
+        </tr>`
+    });
+    text += `</tbody></table><p>Si un mail n'a pas été envoyé la semaine précédente, ce mail contient les statistiques de deux semaines.</p><p>À propos de la <a href="https://fr.wikipedia.org/wiki/Numérotation_ISO_des_semaines">numérotation des semaines</a>.</p>`;
 
     // Get receivers list
     var receivers = [];
