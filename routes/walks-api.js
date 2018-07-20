9b421f189f4c9ca1944711ac484467f870d2c9a1
Cédric JUNG <cedced19@gmail.com>
2018-07-20 14:21:45 +0200

    Associate Walks with Stats

--- routes/walks-api.js
+++ routes/walks-api.js
@@ -23,6 +23,30 @@ router.get('/', function (req, res, next) {
     res.json(req.app.walks.getAll());
 });
 
+/* GET Walks Stats */
+router.get('/stats/', auth, function (req, res, next) {
+    var walks = req.app.walks.getAll();
+    var stats = req.app.stats.getAll();
+    var generated = [];
+    walks.forEach(function (el) {
+        var countApp = 0, countWeb = 0; 
+        if (stats.hasOwnProperty(el.id)) {
+            countApp = stats[el.id];
+        } 
+        if (stats.hasOwnProperty(el.id + '-prev')) {
+            countWeb= stats[el.id + '-prev'];
+        }
+        generated.push({
+            id: el.id,
+            title: el.title,
+            web: countApp,
+            app: countWeb
+        });
+    });
+    res.json(generated);
+});
+
+
 /* GET Walk */
 router.get('/:id', function (req, res, next) {
     req.app.walks.get('id', req.params.id, function (err, model) {
