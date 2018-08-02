fcca2d9e6d31b10887e597ce59c4a0ebc9ebb5c1
Cédric JUNG <cedced19@gmail.com>
2018-08-02 17:27:38 +0200

    Fix sitemap.xml on production

--- routes/index.js
+++ routes/index.js
@@ -9,21 +9,39 @@ router.get('/', function (req, res, next) {
     res.locals.walks = req.app.walks.getAll();
     res.locals.metas = req.app.metas.getAll();
     res.render('index');
-    req.app.stats.add('home', function () {});
+    req.app.stats.add('home', function () { });
 });
 
-/* GET books page */
+/* GET sitemap.xml */
 router.get('/sitemap.xml', function (req, res, next) {
-    res.locals.walks = req.app.walks.getAll();
+    var text = `<?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        <url>
+            <loc>https://decouverto.fr/</loc>
+            <changefreq>weekly</changefreq>
+            <priority>1</priority>
+        </url>
+        <url>
+            <loc>https://decouverto.fr/livres/</loc>
+            <changefreq>monthly</changefreq>
+            <priority>0.8</priority>
+        </url>`
+    req.app.walks.getAll().forEach(function (el) {
+        text += `<url>
+            <loc>https://decouverto.fr/rando/${el.id}/</loc>
+            <changefreq>monthly</changefreq>
+        </url>`;
+    });
+    text += '</urlset>';
     res.contentType('application/xml');
-    res.render('sitemap');
+    res.send(text);
 });
 
 /* GET books page */
 router.get('/livres/', function (req, res, next) {
     res.locals.metas = req.app.metas.getAll();
     res.render('books');
-    req.app.stats.add('books', function () {});
+    req.app.stats.add('books', function () { });
 });
 
 /* GET walk preview page */
@@ -39,7 +57,7 @@ router.get('/rando/:id', function (req, res, next) {
                 res.locals.decompressedFile = false;
             }
             res.render('preview');
-            req.app.stats.add(req.params.id + '-prev', function () {});
+            req.app.stats.add(req.params.id + '-prev', function () { });
         });
     });
 });
